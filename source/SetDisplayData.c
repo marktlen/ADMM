87b5c64517a834592d9cb2d62e3455e43efc3cc6
liang junhao <593044649@qq.com>
2019-05-17 18:46:25 +0800

    create setDisplayData file

---
+++ source/SetDisplayData.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+const unsigned char SmgTab[]={
+	SMG_0,
+	SMG_1,
+	SMG_2,
+	SMG_3,
+	SMG_4,
+	SMG_5,
+	SMG_6,
+	SMG_7,
+	SMG_8,
+	SMG_9,
+	SMG_A,
+	SMG_B,
+	SMG_C,
+	SMG_D,
+	SMG_E,
+	SMG_F,
+	SMG_Off
+};
---
+++ source/SetDisplayData.h
@@ -0,0 +1,20 @@
+#ifndef _SETDISPLAYDATA_H_
+#define _SETDISPLAYDATA_H_
+
+
+#define  LED_AM       PORTA //RC5
+#define  LED_PM       PORTA //RC6
+
+#define  Led_Data1    display2[0]   //数码管1
+#define  Led_Data2    display2[1]   //数码管2
+#define  Led_Data3    display2[2]   //数码管3
+#define  Led_Data4    display2[3]   //数码管4
+
+#define  ON_LED_MudFlash()    Led_Data2 |= (1<<7)
+#define  OFF_LED_MudFlash()   Led_Data2 &= ~(1<<7)
+
+
+void Hex_To_Bcd(unsigned char dispData, unsigned char Class);
+void SetDisplayData();
+
+#endif
